ea2af1d79e7b080dd44aae7807577979278e4187
sr <srzzuares@gmail.com>
2023-12-26 16:45:22 -0600

    img error

--- src/blackjack/useCases/crea-img-html-carta.js
+++ src/blackjack/useCases/crea-img-html-carta.js
@@ -6,7 +6,7 @@
 export const crearCartaHTML = (carta) => {
     if (!carta) throw new Error("Es obligatorio el argumento de la carta");
     const imgCarta = document.createElement('img');
-    imgCarta.src = `public/cartas/${carta}.png`; //3H, JD
+    imgCarta.src = `/public/cartas/${carta}.png`; //3H, JD
     imgCarta.classList.add('carta');
     return imgCarta;
 }
